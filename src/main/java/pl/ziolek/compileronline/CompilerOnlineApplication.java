e18c7cd0defdd75e70236ffca254cd170224edf0
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-10 19:53:09 +0100

    Project start

---
+++ src/main/java/pl/ziolek/compileronline/CompilerOnlineApplication.java
@@ -0,0 +1,14 @@
+package pl.ziolek.compileronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompilerOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompilerOnlineApplication.class, args);
+	}
+
+}
+
