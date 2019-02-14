10ebc746503f901ba175768f24a9131b5611f41b
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-14 11:48:31 +0100

    Added ProgramResults states

--- src/main/java/pl/ziolek/compileronline/program/ProgramResults.java
+++ src/main/java/pl/ziolek/compileronline/program/ProgramResults.java
@@ -1,4 +1,11 @@
 package pl.ziolek.compileronline.program;
 
+import java.util.List;
+
 public class ProgramResults {
+
+    private int id;
+    private List<String> testResults;
+    private List<String> testStatus;
+
 }
