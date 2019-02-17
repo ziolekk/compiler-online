ab0e31c146f69d6f1e7ed3b13692ef990c28dcfd
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-17 13:36:06 +0100

    Implemented Compiler interface

--- src/main/java/pl/ziolek/compileronline/compiler/CppCompiler.java
+++ src/main/java/pl/ziolek/compileronline/compiler/CppCompiler.java
@@ -1,4 +1,12 @@
 package pl.ziolek.compileronline.compiler;
 
-public class CppCompiler {
+import pl.ziolek.compileronline.program.Program;
+import pl.ziolek.compileronline.program.ProgramResults;
+
+public class CppCompiler implements Compiler {
+
+    @Override
+    public ProgramResults compile(Program program) {
+        return null;
+    }
 }
