086f8e271b3fe44545b347b2cdb86c1df9f726ad
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-17 13:38:52 +0100

    Implemented singleton pattern

--- src/main/java/pl/ziolek/compileronline/compiler/CppCompiler.java
+++ src/main/java/pl/ziolek/compileronline/compiler/CppCompiler.java
@@ -5,8 +5,16 @@ import pl.ziolek.compileronline.program.ProgramResults;
 
 public class CppCompiler implements Compiler {
 
+    private CppCompiler instance = new CppCompiler();
+
+    private CppCompiler() {}
+
     @Override
     public ProgramResults compile(Program program) {
         return null;
+    }
+
+    public CppCompiler getInstance() {
+        return instance;
     }
 }
