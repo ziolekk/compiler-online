c2a853f1453be9b74475ce16d4ae8a39b9de0ac3
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-20 16:35:46 +0100

    Added imports

--- src/main/java/pl/ziolek/compileronline/compiler/CppCompiler.java
+++ src/main/java/pl/ziolek/compileronline/compiler/CppCompiler.java
@@ -3,6 +3,9 @@ package pl.ziolek.compileronline.compiler;
 import pl.ziolek.compileronline.program.Program;
 import pl.ziolek.compileronline.program.ProgramResults;
 
+import java.io.File;
+import java.io.IOException;
+
 public class CppCompiler implements Compiler {
 
     private CppCompiler instance = new CppCompiler();
