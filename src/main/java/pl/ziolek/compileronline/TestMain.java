ed34c3bbef9dcf3aa3fcd22d9e840bebf5eb0049
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-21 17:11:53 +0100

    Added getProgram method

--- src/main/java/pl/ziolek/compileronline/TestMain.java
+++ src/main/java/pl/ziolek/compileronline/TestMain.java
@@ -5,4 +5,21 @@ import pl.ziolek.compileronline.program.Program;
 import java.util.ArrayList;
 
 public class TestMain {
+
+    public static void main(String[] args) {
+
+    }
+
+    private Program getProgram() {
+        return new Program(
+                1,
+                "#include <iostream>" +
+                        "using namespace std;" +
+                        "" +
+                        "int main() {" +
+                        "   cout << \"Hello World\";" +
+                        "}",
+                new ArrayList<>()
+        );
+    }
 }
