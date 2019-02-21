37a7cd4c8967f5111b896e9f22dad968b4ccb5aa
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-21 17:14:29 +0100

    Create cppCompiler state

--- src/main/java/pl/ziolek/compileronline/TestMain.java
+++ src/main/java/pl/ziolek/compileronline/TestMain.java
@@ -1,13 +1,16 @@
 package pl.ziolek.compileronline;
 
+import pl.ziolek.compileronline.compiler.CppCompiler;
 import pl.ziolek.compileronline.program.Program;
 
 import java.util.ArrayList;
 
 public class TestMain {
 
-    public static void main(String[] args) {
+    private CppCompiler cppCompiler;
 
+    public static void main(String[] args) {
+        cppCompiler = CppCompiler.getInstance();
     }
 
     private Program getProgram() {
