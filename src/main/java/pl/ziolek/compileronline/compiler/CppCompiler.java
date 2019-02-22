a0c182b8612f0a70003a2ab8b95c0e7ba8945056
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-22 15:28:30 +0100

    Fixed command, remove spaces from end

--- src/main/java/pl/ziolek/compileronline/compiler/CppCompiler.java
+++ src/main/java/pl/ziolek/compileronline/compiler/CppCompiler.java
@@ -10,11 +10,11 @@ public class CppCompiler implements Compiler {
     private static CppCompiler instance = new CppCompiler();
     private ProgramResults programResults;
     private final String pathToFile = "/home/ziolek/Projects/IdeaProjects/CompilerOnline/cpp_compilations/";
-    private final String fileName = "code.cpp ";
-    private final String compileFileName = "program ";
-    private final String cleanUpCommand = "rm ";
-    private final String compileCommand1 = "g++ ";
-    private final String compileCommand2 = "-o ";
+    private final String fileName = "code.cpp";
+    private final String compileFileName = "program";
+    private final String cleanUpCommand = "rm";
+    private final String compileCommand1 = "g++";
+    private final String compileCommand2 = "-o";
 
     private CppCompiler() {}
 
@@ -52,12 +52,12 @@ public class CppCompiler implements Compiler {
 
     private boolean compileProgram() throws IOException, InterruptedException {
         StringBuffer output = new StringBuffer();
-        System.out.println("Execute command: " + this.compileCommand1 + this.pathToFile + this.fileName
-                + this.compileCommand2 + this.pathToFile + this.compileFileName
+        System.out.println("Execute command: " + this.compileCommand1 + " " + this.pathToFile + this.fileName
+                + " " +  this.compileCommand2 + " " + this.pathToFile + this.compileFileName
         );
         Process process = Runtime.getRuntime().exec(
-                this.compileCommand1 + this.pathToFile + this.fileName
-                + this.compileCommand2 + this.pathToFile + this.compileFileName
+                this.compileCommand1 + " " + this.pathToFile + this.fileName
+                + " " +  this.compileCommand2 + " " + this.pathToFile + this.compileFileName
         );
         process.waitFor();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -77,8 +77,8 @@ public class CppCompiler implements Compiler {
     }
 
     private void cleanUp() throws IOException, InterruptedException {
-        System.out.println("Exec command: " + this.cleanUpCommand + this.pathToFile + this.fileName);
-        Process process = Runtime.getRuntime().exec(this.cleanUpCommand + this.pathToFile + this.fileName);
+        System.out.println("Exec command: " + this.cleanUpCommand + " " + this.pathToFile + this.fileName);
+        Process process = Runtime.getRuntime().exec(this.cleanUpCommand + " " + this.pathToFile + this.fileName);
         process.waitFor();
     }
 }
