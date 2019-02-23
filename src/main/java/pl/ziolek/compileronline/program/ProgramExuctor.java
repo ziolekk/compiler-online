1615f4b5099329fec2dfe4fc6b504a23460485a3
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-23 13:47:02 +0100

    Implemented singleton pattern

--- src/main/java/pl/ziolek/compileronline/program/ProgramExuctor.java
+++ src/main/java/pl/ziolek/compileronline/program/ProgramExuctor.java
@@ -1,4 +1,12 @@
 package pl.ziolek.compileronline.program;
 
 public class ProgramExuctor {
+
+    private static ProgramExuctor instance = new ProgramExuctor();
+
+    private ProgramExuctor() {}
+
+    public static ProgramExuctor getInstance() {
+        return instance;
+    }
 }
