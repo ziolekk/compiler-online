0de657f0222e33a47fcc3c031121195ee98d621b
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-23 14:03:06 +0100

    Added private states and constructor

--- src/main/java/pl/ziolek/compileronline/program/SingleTestResult.java
+++ src/main/java/pl/ziolek/compileronline/program/SingleTestResult.java
@@ -1,4 +1,12 @@
 package pl.ziolek.compileronline.program;
 
 public class SingleTestResult {
+
+    private String result;
+    private ResultStatus resultStatus;
+
+    public SingleTestResult(String result, ResultStatus resultStatus) {
+        this.result = result;
+        this.resultStatus = resultStatus;
+    }
 }
