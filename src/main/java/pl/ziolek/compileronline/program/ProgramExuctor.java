8d6ab9e122bc7f7a8077b7e9a6af63a867f5e130
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-23 14:23:27 +0100

    Added private final state

--- src/main/java/pl/ziolek/compileronline/program/ProgramExuctor.java
+++ src/main/java/pl/ziolek/compileronline/program/ProgramExuctor.java
@@ -6,6 +6,11 @@ public class ProgramExuctor {
 
     private static ProgramExuctor instance = new ProgramExuctor();
 
+    private final String PATH_TO_FOLDER = "/home/ziolek/Projects/IdeaProjects/CompilerOnline/cpp_compilations/";
+    private final String TEST_FILE_NAME = "input.in";
+    private final String ABSOLUTE_PATH_TO_FILE = PATH_TO_FOLDER + TEST_FILE_NAME;
+    private final String CLEAN_UP_METHOD = "rm " + PATH_TO_FOLDER + TEST_FILE_NAME;
+
     private ProgramExuctor() {}
 
     public static ProgramExuctor getInstance() {
