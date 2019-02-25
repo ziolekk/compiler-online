8175b4d2ff4f93c70723cbdf0ab7b2c79a9796fc
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-25 17:34:36 +0100

    Added runProgram.sh script to execute program with file input

---
+++ cpp_compilations/runProgram.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/ziolek/Projects/IdeaProjects/CompilerOnline/cpp_compilations/program < /home/ziolek/Projects/IdeaProjects/CompilerOnline/cpp_compilations/input.in
+#ls /home/ziolek/Projects/IdeaProjects/CompilerOnline/cpp_compilations/
