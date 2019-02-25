65f5b0b2944cc9f562e22aa002856d4c770d77f2
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-25 16:42:18 +0100

    Updated TestMain

--- src/main/java/pl/ziolek/compileronline/TestMain.java
+++ src/main/java/pl/ziolek/compileronline/TestMain.java
@@ -15,13 +15,13 @@ public class TestMain {
 
         ProgramResults results = cppCompiler.compile(program);
 
-        System.out.println(results.toString());
+
     }
 
     public Program getProgram() {
 
         ArrayList tests = new ArrayList<>();
-        tests.add(" ");
+        tests.add("69");
 
         return new Program(
                 1,
@@ -30,6 +30,7 @@ public class TestMain {
                         "\n" +
                         "int main() {\n" +
                         "   cout << \"Hello World\";\n" +
+                        "int x; cin >> x; cout << x << endl;" +
                         "}\n",
                 tests
         );
