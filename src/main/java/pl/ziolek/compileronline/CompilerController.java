31580514d7b951dbce5f825ee3b950669037a21c
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-26 16:43:41 +0100

    Added getResultsFromProgram method wiht @RequestMapping

--- src/main/java/pl/ziolek/compileronline/CompilerController.java
+++ src/main/java/pl/ziolek/compileronline/CompilerController.java
@@ -1,11 +1,21 @@
 package pl.ziolek.compileronline;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.ziolek.compileronline.program.Program;
+import pl.ziolek.compileronline.program.ProgramResults;
 
 @RestController
 @RequestMapping(value = "/")
 public class CompilerController {
 
+    @RequestMapping(value = "/compile", method = RequestMethod.GET)
+    public ResponseEntity<ProgramResults> getResultsFromProgram(@RequestBody Program program) {
 
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }
