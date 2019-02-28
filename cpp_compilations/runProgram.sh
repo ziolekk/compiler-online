f3e1f448291002e52d777733cf32f0b62f4563a0
ziolek <ziolkowskiwiktor99@gmail.com>
2019-02-28 10:02:47 +0100

    Updated runProgram.sh, remove odl command

--- cpp_compilations/runProgram.sh
+++ cpp_compilations/runProgram.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 /home/ziolek/Projects/IdeaProjects/CompilerOnline/cpp_compilations/program < /home/ziolek/Projects/IdeaProjects/CompilerOnline/cpp_compilations/input.in
-#ls /home/ziolek/Projects/IdeaProjects/CompilerOnline/cpp_compilations/
