34e14f8eaf5a23be65d998a83ab0250eaaea1757
ziolek <ziolkowskiwiktor99@gmail.com>
2019-03-03 18:42:18 +0100

    Added maxExecutionTimeForTestInSeconds state in Program

--- src/main/java/pl/ziolek/compileronline/program/Program.java
+++ src/main/java/pl/ziolek/compileronline/program/Program.java
@@ -7,11 +7,13 @@ public class Program {
     private int id;
     private String code;
     private List<String> tests;
+    private List<Integer> maxExecutionTimeForTestInSeconds;
 
-    public Program(int id, String code, List<String> tests) {
+    public Program(int id, String code, List<String> tests, List<Integer> maxExecutionTimeForTestInSeconds) {
         this.id = id;
         this.code = code;
         this.tests = tests;
+        this.maxExecutionTimeForTestInSeconds = maxExecutionTimeForTestInSeconds;
     }
 
     public int getId() {
