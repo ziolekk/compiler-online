0f0ef1e66b75f90d19f1409fa002ed5e2173fda8
ziolek <ziolkowskiwiktor99@gmail.com>
2019-03-18 13:22:25 +0100

    Added CrossOrigin anaotations in Controllers

--- src/main/java/pl/ziolek/compileronline/CompilerController.java
+++ src/main/java/pl/ziolek/compileronline/CompilerController.java
@@ -2,14 +2,12 @@ package pl.ziolek.compileronline;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.ziolek.compileronline.compiler.CppCompiler;
 import pl.ziolek.compileronline.program.Program;
 import pl.ziolek.compileronline.program.ProgramResults;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/")
 public class CompilerController {
--- src/main/java/pl/ziolek/compileronline/TestController.java
+++ src/main/java/pl/ziolek/compileronline/TestController.java
@@ -1,9 +1,11 @@
 package pl.ziolek.compileronline;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
